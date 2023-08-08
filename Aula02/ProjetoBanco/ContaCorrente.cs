a9b5567153200882c487e721b79bb5d1529e4e9b
Bianca-karoline <113278939+Bianca-karoline@users.noreply.github.com>
2023-08-08 12:09:14 -0300

    Add files via upload

---
+++ Aula02/ProjetoBanco/ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBanco
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public int numero;
+        public int agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                Console.WriteLine("Saldo indisponivel");
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+                Console.WriteLine("Deposito realizado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Deposito invalido");
+            }
+        }
+    }
+}
---
+++ Aula02/ProjetoBanco/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBanco
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+            c1.titular = "12345678900";
+            c1.saldo = 100;
+            Console.WriteLine("Digite um valor para sacar");
+            c1.Sacar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(c1.saldo);
+            c1.Depositar(30);
+            Console.WriteLine(c1.saldo);
+            Console.ReadLine();
+            
+        }
+    }
+    
+}
