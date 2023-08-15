a7e27c160823b500a2cfe4c6e49176806f92af32
Bianca-karoline <113278939+Bianca-karoline@users.noreply.github.com>
2023-08-14 21:29:43 -0300

    Add files via upload

---
+++ aula03/aula03/ContaCorrente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula03
+{
+    class ContaCorrente
+    {
+        public Cliente titular;
+        public int numero;
+        public int agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                Console.WriteLine("Saldo indisponivel");
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+                Console.WriteLine("Deposito realizado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Deposito invalido");
+            }
+        }
+        public void Pix(ContaCorrente c, double valor)
+        {
+            if (valor <= saldo)
+            {
+                c.Depositar(valor);
+                this.Sacar(valor);
+            }
+            else
+            {
+                Console.WriteLine("Erro ao realizar transferencia");
+            }
+            
+
+        }
+    }
+}
---
+++ aula03/aula03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+            ContaCorrente c2 = new ContaCorrente();
+            c1.saldo = 100;
+            c2.saldo = 20;
+            c1.Pix(c2, 10);
+            Console.WriteLine(c2.saldo);
+            Console.ReadLine();
+            
+        }
+    }
+}
